9552e6f95fc464563b06c18e7da558316c763201
lgrajeda <lmgrajeda@gmail.com>
2015-07-22 08:50:16 -0600

    Caching the inverse of a matrix.
    R Programming
    programming assigment 2.
    Caching the inverse of a matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,64 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming assingment 2
+## this code caches the inverse of a matrix in order to 
+## avoid computing it repeatedly  when doing for example
+## a loop.
 
-## Write a short comment describing this function
+
+
+## this function makes a list 4 elemtns, the first set the 
+## value of the matrix, the secodn get the value of the matrix,
+## the third set the value of the inverse and the last get the 
+## value of the inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i<-NULL
+        set<-function(y){
+                x<<-y
+                i<<-NULL
+                }
+        get<-function()x
+        setinverse<-function(solve)i<<-solve
+        getinverse<-function()i
+        list(set=set, get=get,
+             setinverse=setinverse,
+             getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## this function takes the list created above and calculates
+## the inverse of the matrix. But before calculating it
+## look for the solution in the cache memory. The solution
+## will be in the cache if it was calculated before.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i<-x$getinverse()
+        if(!is.null(i)){
+                message("getting cached data")
+                return(i)
+        }
+        data<-x$get()
+        i<-solve(data, ...)
+        x$setinverse(i)
+        i
 }
+
+
+
+###############################################################
+## Example
+z<-matrix(c(4, 2, 7, 6), 2, 2) #to create a matrix
+z
+solve(z) #to calculate the inverse of the matrix directly
+
+y<-makeCacheMatrix(z) #creates the list
+y
+cacheSolve(y) #to calculate the inverse using the function
+
+cacheSolve(y) #run the function again, notice the message
+#"getting cached data" because the solution was pulled from
+#the cache memory rather than calculate it again.
+
+
+
+##THE END
+#######
